f84690c1c287e3bbbea86689fa5885384b8aee3d
aman6ig <aman6ig@outlook.com>
2025-09-08 15:38:51 +0530

    Update handleCreateDatabase.js

--- includes/handle/handleCreateDatabase.js
+++ includes/handle/handleCreateDatabase.js
@@ -1,62 +1,89 @@
 module.exports = function ({ Users, Threads, Currencies }) {
-    const logger =require("../../utils/log.js");
+    const logger = require("../../utils/log.js");
+    
     return async function ({ event }) {
         const { allUserID, allCurrenciesID, allThreadID, userName, threadInfo } = global.data; 
         const { autoCreateDB } = global.config;
-        if (autoCreateDB == ![]) return;
+        
+        if (autoCreateDB == false) return;
+        
         var { senderID, threadID } = event;
         senderID = String(senderID);
-        var threadID = String(threadID);
+        threadID = String(threadID);
+        
         try {
-            if (!allThreadID.includes(threadID) && event.isGroup == !![]) {
+            // Create thread data if not exists
+            if (!allThreadID.includes(threadID) && event.isGroup == true) {
                 const threadIn4 = await Threads.getInfo(threadID);
-                const setting = {};
-                setting.threadName = threadIn4.threadName
-                setting.adminIDs = threadIn4.adminIDs
-                setting.nicknames = threadIn4.nicknames;
+                
+                const setting = {
+                    threadName: threadIn4.threadName,
+                    adminIDs: threadIn4.adminIDs,
+                    nicknames: threadIn4.nicknames
+                };
+                
                 const dataThread = setting;
-                allThreadID.push(threadID)
+                allThreadID.push(threadID);
                 threadInfo.set(threadID, dataThread);
-                const setting2 = {};
-                setting2.threadInfo = dataThread
-                setting2.data = {}
+                
+                const setting2 = {
+                    threadInfo: dataThread,
+                    data: {}
+                };
+                
                 await Threads.setData(threadID, setting2);
-                for (singleData of threadIn4.userInfo) {
+                
+                // Create user data for all thread members
+                for (const singleData of threadIn4.userInfo) {
                     userName.set(String(singleData.id), singleData.name);
+                    
                     try {
-                        global.data.allUserID.includes(String(singleData.id)) ? (await Users.setData(String(singleData.id), 
-                        {
-                            'name': singleData.name
-                        }), 
-                        global.data.allUserID.push(singleData.id)) : (await Users.createData(singleData.id, 
-                        {
-                            'name': singleData.name,
-                            'data': {}
-                        }), 
-                        global.data.allUserID.push(String(singleData.id)), 
-                        logger(global.getText('handleCreateDatabase', 'newUser', singleData.id), '[ DATABASE ]'));
-                    } catch(e) { console.log(e) };
+                        if (global.data.allUserID.includes(String(singleData.id))) {
+                            await Users.setData(String(singleData.id), {
+                                'name': singleData.name
+                            });
+                        } else {
+                            await Users.createData(singleData.id, {
+                                'name': singleData.name,
+                                'data': {}
+                            });
+                            global.data.allUserID.push(String(singleData.id));
+                            logger(global.getText('handleCreateDatabase', 'newUser', singleData.id), '[ DATABASE ]');
+                        }
+                    } catch(e) { 
+                        console.log('User creation error:', e);
+                    }
                 }
+                
                 logger(global.getText('handleCreateDatabase', 'newThread', threadID), '[ DATABASE ]');
             }
+            
+            // Create user data if not exists
             if (!allUserID.includes(senderID) || !userName.has(senderID)) {
-                const infoUsers = await Users.getInfo(senderID),
-                    setting3 = {};
-                setting3.name = infoUsers.name
-                await Users.createData(senderID, setting3)
-                allUserID.push(senderID) 
-                userName.set(senderID, infoUsers.name)
+                const infoUsers = await Users.getInfo(senderID);
+                const setting3 = {
+                    name: infoUsers.name
+                };
+                
+                await Users.createData(senderID, setting3);
+                allUserID.push(senderID);
+                userName.set(senderID, infoUsers.name);
                 logger(global.getText('handleCreateDatabase', 'newUser', senderID), '[ DATABASE ]');
             }
+            
+            // Create currency data if not exists
             if (!allCurrenciesID.includes(senderID)) {
-                const setting4 = {};
-                setting4.data = {}
-                await Currencies.createData(senderID, setting4) 
+                const setting4 = {
+                    data: {}
+                };
+                
+                await Currencies.createData(senderID, setting4);
                 allCurrenciesID.push(senderID);
             }
+            
             return;
         } catch (err) {
-            return console.log(err);
+            console.log('Database creation error:', err);
         }
     };
-}
+};
